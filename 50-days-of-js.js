c40b934ecaf5b2a6fd2d81a84774e9b124c63ad0
Jogata <53145312+Jogata@users.noreply.github.com>
2021-12-21 01:13:50 +0200

    add challenge #3 in 50-days-of-js

--- 50-days-of-js.js
+++ 50-days-of-js.js
@@ -28,3 +28,22 @@ function reverseAString(str) {
 }
 
 console.log(reverseAString(str));
+
+
+// #3. Write a program to reverse a given integer number
+// The remainder of the number can be fetched and the number can be divided by 10 to remove the the digit in loop till number becomes 0
+// A simple approach to reverse a number could also be to convert it in to a string and then reverse it
+// reverseGivenInteger(3849) returns 9483
+// reverseGivenInteger(2222) returns 2222
+// reverseGivenInteger(1002) returns 2001
+
+const num = 3849;
+
+function reverseGivenInteger(num) {
+    let a = num.toString();
+    a = a.split("").reverse().join("");
+    return parseFloat(a);
+}
+
+let num1 = reverseGivenInteger(num)
+console.log(num1)
