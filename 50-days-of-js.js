0ee017599b5d249ac8e460d246f42f20da693747
Jogata <53145312+Jogata@users.noreply.github.com>
2021-12-20 02:53:08 +0200

    add challenge #2 in 50-days-of-js

--- 50-days-of-js.js
+++ 50-days-of-js.js
@@ -4,9 +4,27 @@
 // randomNumberGeneratorInRange(100, 200) should return a number between 100-200 (inclusive)
 
 function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
-	const rand = Math.random() * (rangeEnd - rangeStart) + rangeStart;
-
-	return rand;
+  const rand = Math.random() * (rangeEnd - rangeStart) + rangeStart;
+  return rand;
 }
 
 console.log(`My random number: ${randomNumberGeneratorInRange(5, 100)}`)
+
+
+// #2. Write a program to reverse a string
+// String can be reversed by iterating it and storing it in reverse order
+// String can also be reversed by converting it to array, then joining it after reversing
+// reverseAString("JavaScript is awesome") should return "emosewa si tpircSavaJ"
+// reverseAString("Peter Parker is Spiderman") should return "namredipS si rekraP reteP"
+// reverseAString("codedamn") should return "nmadedoc"
+
+let str = "JavaScript is awesome"
+
+function reverseAString(str) {
+  if (str === "")
+    return "";
+  else
+    return reverseAString(str.substr(1)) + str.charAt(0);
+}
+
+console.log(reverseAString(str));
