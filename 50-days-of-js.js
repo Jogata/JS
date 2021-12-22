f5267c216096f4d15f62f38e4e6a0eca41582b85
Jogata <53145312+Jogata@users.noreply.github.com>
2021-12-22 19:03:31 +0200

    add challenge #8 in 50-days-of-js

--- 50-days-of-js.js
+++ 50-days-of-js.js
@@ -140,4 +140,24 @@ function validateMobile(number) {
 console.log(`is a valid Indian mobile number: ${validateMobile(number)}`)
 
 
+// #8. Write a function which accepts two valid dates and returns the difference between them as number of days
+// The difference between 2 dates in JavaScript will give the time difference in milliseconds
+// Time difference can be converted in to days by dividing the 24Hrs time in milliseconds
+// getDaysBetweenDates('10/15/2020', '12/1/2020') returns 47
+// getDaysBetweenDates('11/10/2021', '11/12/2021') returns 2
+// getDaysBetweenDates('11/01/2020', '11/05/2020') returns 4
 
+
+function getDaysBetweenDates(dateText1, dateText2) {
+    const DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
+    const firstDate = new Date(dateText1).getTime();
+    const secondDate = new Date(dateText2).getTime();
+
+    const timeLapseInMiliseconds = firstDate - secondDate;
+    const daysBetweenTwoDates = Math.floor(Math.abs(timeLapseInMiliseconds / DAY_IN_MILLISECONDS));
+
+    return daysBetweenTwoDates
+}
+
+console.log(`Days difference: ${getDaysBetweenDates('10/15/2020', '12/1/2020')}`) //47
+console.log(`Days difference: ${getDaysBetweenDates('11/01/2020', '11/05/2020')}`) //4
