94695c793eda449caf50bf237bc72a349a2442f1
Jogata <53145312+Jogata@users.noreply.github.com>
2021-12-22 14:06:26 +0200

    add challenge #7 in 50-days-of-js

--- 50-days-of-js.js
+++ 50-days-of-js.js
@@ -117,3 +117,27 @@ function truncateWithWordLimit(str, wordLimit) {
 }
 
 console.log(truncateWithWordLimit(str, wordLimit))
+
+
+// #7. Create a regular expression to validate if the given input is valid Indian mobile number or not
+// Regular expression check has to have an optional +91 or 0 in the beginning, then an optional space and 10 digits
+// test method of regular expression can be used to validate if the mobile number pattern matches or not
+// validateMobile('+919876543210') returns true
+// validateMobile('+91 9876543210') returns true
+// validateMobile('09876543210') returns true
+// validateMobile('9876543210') returns true
+// validateMobile('99876543210') returns false
+
+const number = '+919876543210';
+
+function validateMobile(number) {
+    let pattern = /^((\+91|0)|(\+91|0) )?[0-9]{10}$/g;
+    let isValid = pattern.test(number);
+    
+    return isValid
+}
+
+console.log(`is a valid Indian mobile number: ${validateMobile(number)}`)
+
+
+
