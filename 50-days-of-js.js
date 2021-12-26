b94bfd34c509647208b5b2bb446139fdb66f0f0a
Jogata <53145312+Jogata@users.noreply.github.com>
2021-12-26 10:53:26 +0200

    add challenge #23 in 50-days-of-js

--- 50-days-of-js.js
+++ 50-days-of-js.js
@@ -425,3 +425,42 @@ let uniqueInOrder = (iterable) => {
 };
 
 console.log(uniqueInOrder([1,2,2,3,3])); // => [1,2,3]
+
+
+// #23. Equal Sides Of An Array
+// You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+// Let's say you are given the array [1,2,3,4,3,2,1]: Your function will return the index 3, because at the 3rd position of the array, the sum of left side of the index ([1,2,3]) and the sum of the right side of the index ([3,2,1]) both equal 6.
+// Let's look at another one. You are given the array [1,100,50,-51,1,1] Your function will return the index 1, because at the 1st position of the array, the sum of left side of the index ([1]) and the sum of the right side of the index ([50,-51,1,1]) both equal 1.
+// Last one: You are given the array [20,10,-80,10,10,15,35] At index 0 the left side is [] The right side is [10,-80,10,10,15,35] They both are equal to 0 when added. (Empty arrays are equal to 0 in this problem) Index 0 is the place where the left side and right side are equal.
+// Input: An integer array of length 0 < arr < 1000. The numbers in the array can be any integer positive or negative.
+// Output: The lowest index N where the side to the left of N is equal to the side to the right of N. If you do not find an index that fits these rules, then you will return -1.
+// Note: If you are given an array with multiple answers, return the lowest correct index.
+// findEvenIndex([1,2,3,4,3,2,1] should return 3
+// findEvenIndex([1,100,50,-51,1,1]) should return 1
+// findEvenIndex([1,2,3,4,5,6]) should return -1
+// findEvenIndex([20,10,30,10,10,15,35]) should return 3
+
+function findEvenIndex(arr) {
+    let response = -1;
+
+    for (let i = 0; i < arr.length; i++){
+        let leftSum = null;
+        let rightSum = null;
+        // sum on the left side of the current index(i)
+        for (let l = 0; l < i ; l++){
+            leftSum += arr[l];
+        }
+        // sum on the right side of the current index(i)
+        for (let j = i+1; j < arr.length; j++){
+            rightSum += arr[j];
+        }
+
+        if (leftSum === rightSum){
+            response = i;
+        }
+    }
+
+    return response
+}
+
+console.log(findEvenIndex([20,10,30,10,10,15,35])); // => 3
