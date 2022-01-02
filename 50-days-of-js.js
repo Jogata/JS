9dd35454cc1027dfaf90e72b15b0ba899a4ab7fd
Jogata <53145312+Jogata@users.noreply.github.com>
2022-01-02 12:47:07 +0200

    add challenge #42 in 50-days-of-js

--- 50-days-of-js.js
+++ 50-days-of-js.js
@@ -1097,3 +1097,20 @@ function openOrSenior(data) {
 const input = [[45, 12],[55,21],[19, -2],[104, 20]];
   
 console.log(openOrSenior(input));
+
+
+// #42. Sum of two lowest positive integers
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+// sumTwoSmallestNumbers([5, 8, 12, 19, 22]) should return 13
+// sumTwoSmallestNumbers([15, 28, 4, 2, 43]) should return 6
+// sumTwoSmallestNumbers([23, 71, 33, 82, 1]) should return 24
+
+function sumTwoSmallestNumbers(numbers) {
+    numbers.sort(function(a, b){return a-b});
+    const sumOfTwoSmallestNumbers = numbers[0] + numbers[1];
+    return sumOfTwoSmallestNumbers
+}
+
+sumTwoSmallestNumbers([5, 8, 12, 19, 22]);
+
