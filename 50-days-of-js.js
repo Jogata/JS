f53bb52ccab43556e5d88386d248d2e5b6e40509
Jogata <53145312+Jogata@users.noreply.github.com>
2022-01-03 17:21:02 +0200

    add challenge #46 in 50-days-of-js

--- 50-days-of-js.js
+++ 50-days-of-js.js
@@ -1228,3 +1228,28 @@ console.log(findOutlier([0,0,3,0,0]));  // 3
 console.log(findOutlier([1,1,0,1,1]));  // 0
 console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36])); // 11
 console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21])); // 160
+
+
+// #46. A Needle in the Haystack
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+// After your function finds the needle it should return a message (as a string) that says:
+// "found the needle at position " plus the index it found the needle, so:
+// findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']) should return "found the needle at position 5"
+// findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]) should return 'found the needle at position 3'
+// findNeedle(['283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago']) should return 'found the needle at position 5'
+// findNeedle([1,2,3,4,5,6,7,8,8,7,5,4,3,4,5,6,67,5,5,3,3,4,2,34,234,23,4,234,324,324,'needle',1,2,3,4,5,5,6,5,4,32,3,45,54]) should return 'found the needle at position 30'
+
+function findNeedle(haystack) {
+    let needlePosition = null;
+    
+    needlePosition = haystack.indexOf('needle');
+
+    let response = `found the needle at position ${needlePosition}`;
+
+    return response
+}
+
+console.log(findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])); // 5
+console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false])); // 3
+console.log(findNeedle(['283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago'])); // 5
+console.log(findNeedle([1,2,3,4,5,6,7,8,8,7,5,4,3,4,5,6,67,5,5,3,3,4,2,34,234,23,4,234,324,324,'needle',1,2,3,4,5,5,6,5,4,32,3,45,54])); // 30
