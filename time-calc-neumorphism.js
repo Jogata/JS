13bd528e6779c04bdae86fba200c22efcceeaaf5
Yonko Nikolov <jonkoslav@abv.bg>
2022-04-06 16:53:17 +0300

    Time Calculator With Neumorphism Design

---
+++ time-calc-neumorphism.js
@@ -0,0 +1,26 @@
+
+
+/* ==============   HTML   ============== 
+    <section class="calculator">
+        <div class="inputs">
+            <input type="date" name="date" id="date">
+            <button onclick="calcTimeInterval()">Calculate</button>
+        </div>
+        <div class="outputs">
+            <div>
+                <span id="years">-</span>
+                <p>years</p>
+            </div>
+            <div>
+                <span id="months">-</span>
+                <p>months</p>
+            </div>
+            <div>
+                <span id="days">-</span>
+                <p>days</p>
+            </div>
+        </div>
+    </section>
+*/
+/* ==============   CSS   ============== 
+*/
