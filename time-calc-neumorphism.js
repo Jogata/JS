8249bef4d144fea9b7e7e55a3d8c8860e81e2c87
Yonko Nikolov <jonkoslav@abv.bg>
2022-04-06 18:03:10 +0300

    Time Calculator With Neumorphism Design

--- time-calc-neumorphism.js
+++ time-calc-neumorphism.js
@@ -23,4 +23,70 @@
     </section>
 */
 /* ==============   CSS   ============== 
+        body {
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: #e8eaec;
+        }
+
+        .calculator {
+            width: 40%;
+            min-width: 450px;
+            padding: 50px 30px;
+        }
+
+        .inputs {
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+            margin-bottom: 40px;
+            padding: 25px;
+            box-shadow: 
+            4px 4px 8px rgb(189, 200, 213),
+            -4px -4px 8px rgb(255, 255, 255);
+        }
+        .inputs input,
+        .inputs button {
+            height: 40px;
+            color: #080808;
+            padding: 0 20px;
+            outline: none;
+            border: none;
+            border-radius: 10px;
+            background-color: inherit;
+        }
+        .inputs input,
+        .inputs button:active {
+            box-shadow: 
+            inset 4px 4px 8px rgb(189, 200, 213),
+            inset -4px -4px 8px rgb(255, 255, 255);
+        }
+        .inputs input {
+            width: 60%;
+        }
+        .inputs button {
+            cursor: pointer;
+        }
+
+        .outputs {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            text-align: center;
+        }
+        .outputs div {
+            width: 100px;
+            height: 100px;
+            display: grid;
+            place-items: center;
+            padding: 20px 0;
+            box-shadow: 
+            4px 4px 8px rgb(189, 200, 213),
+            -4px -4px 8px rgb(255, 255, 255),
+            inset 4px 4px 8px rgb(189, 200, 213),
+            inset -2px -2px 8px rgb(255, 255, 255);
+            border-radius: 10px;
+        }
 */
