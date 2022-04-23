73677fea0a108c972ce2f3d14980146f8d17346c
Yonko Nikolov <jonkoslav@abv.bg>
2022-04-23 09:23:47 +0300

    Hamburger Menu Examples

--- tutorials/hamburger-menus.js
+++ tutorials/hamburger-menus.js
@@ -74,3 +74,84 @@ hamburger.addEventListener('click', () => {
     transition: top 0.25s ease-in-out, transform 0.25s ease-in-out 0.25s;
 }
 */
+
+
+
+const hamburger2 = document.querySelector('.menu-toggle');
+
+hamburger2.addEventListener('click', () => {
+    hamburger2.classList.toggle('active');
+})
+/* ==============   HTML   ============== 
+    <div class="menu-toggle">
+        <div class="hamburger">
+            <span></span>
+        </div>
+    </div>
+
+<script src="../JS/tutorials/hamburger-menus.js"></script>
+*/
+
+/* ==============   CSS   ============== 
+*, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.menu-toggle {
+	position: fixed;
+	top: 2rem;
+	right: 2rem;
+	width: 60px;
+	height: 60px;
+	border-radius: 99px;
+	background-color: #2e3047;
+	cursor: pointer;
+}
+
+.hamburger {
+	position: relative;
+	top: calc(50% - 2px);
+	left: 50%;
+	transform: translate(-50%, -50%);
+	width: 32px;
+}
+
+.hamburger > span,
+.hamburger > span::before,
+.hamburger > span::after {
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 4px;
+    border-radius: 99px;
+    background-color: #FFF;
+    transition-duration: .25s;
+}
+
+.hamburger > span::before {
+    content: '';
+    top: -8px;
+}
+.hamburger > span::after {
+    content: '';
+    top: 8px;
+}
+
+.menu-toggle.active .hamburger {
+    left: calc(50% + 2px);
+}
+.menu-toggle.active .hamburger > span {
+    transform: rotate(45deg);
+    height: 0px;
+}
+.menu-toggle.active .hamburger > span::before {
+    top: 0;
+    transform: rotate(0deg);
+}
+.menu-toggle.active .hamburger > span::after {
+    top: 0;
+    transform: rotate(90deg);
+}
+*/
